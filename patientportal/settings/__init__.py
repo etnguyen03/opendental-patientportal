5f31d78dffd71fd3a4e0f69c0550e7000856f32b
Ethan Nguyen <etnguyen03@hotmail.com>
2020-08-15 23:12:02 -0400

    fix(ci): fix settings import fail

--- patientportal/settings/__init__.py
+++ patientportal/settings/__init__.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
-from .config import *
+try:
+    from .config import *
+except ModuleNotFoundError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
